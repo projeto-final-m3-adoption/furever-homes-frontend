58724a29567194217f7134aa06393514f6f89749
Mateus João Feldhaus <mjf@tutamail.com>
2023-05-02 14:27:07 -0300

    feat: providers

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { GlobalStyle } from "./styles/GlobalStyles";
 import { MainRoutes } from "./routes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { UserProvider } from "./providers/userContext";
 
 export function App() {
   return (
     <>
       <GlobalStyle />
       <ToastContainer />
-      <MainRoutes />
+      <UserProvider>
+        <MainRoutes />
+      </UserProvider>
     </>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,27 +3,14 @@ import { Login } from "../pages/Login";
 import { Register } from "../pages/Register";
 import { Home } from "../pages/Home";
 import { PageNotFound } from "../pages/PageNotFound";
-import { UserProvider } from "../providers/userContext";
 
 export function MainRoutes() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-<<<<<<< HEAD
-      <Route
-        path="/register"
-        element={
-          <UserProvider>
-            <Register />
-          </UserProvider>
-        }
-      />
-      <Route path="/" element={<Navigate to="/login" />} />
-=======
       <Route path="/register" element={<Register />} />
       <Route path="/home" element={<Home />} />
       <Route path="/" element={<Navigate to="/home" />} />
->>>>>>> 1cd9b7ff63dab9e96cc2faca69f0e3f2a2c9a957
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
