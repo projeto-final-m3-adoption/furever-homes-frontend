1cd9b7ff63dab9e96cc2faca69f0e3f2a2c9a957
bitoller <bi_toller@hotmail.com>
2023-05-02 14:12:37 -0300

    feat: add toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { GlobalStyle } from "./styles/GlobalStyles";
 import { MainRoutes } from "./routes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export function App() {
   return (
     <>
       <GlobalStyle />
+      <ToastContainer />
       <MainRoutes />
     </>
   );
