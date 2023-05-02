e371f69726fb150ef59dce7734f3f0524d7eadf8
Bianca <115846023+bitoller@users.noreply.github.com>
2023-05-02 16:19:18 -0300

    Revert "Feature petcontext"

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App.tsx";
-import { PetProvider } from "./providers/petContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <PetProvider>
-        <App />
-      </PetProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
