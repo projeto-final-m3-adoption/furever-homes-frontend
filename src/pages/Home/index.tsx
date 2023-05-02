c0acb48651d3a7e191ee319faac089d00ebfbe51
Amanda Dolci <amanda.dolci@gmail.com>
2023-05-02 12:59:18 -0300

    feat: add homepage with header component

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import { Header } from "../../components/Header";
+import { StyledHome } from "./style";
+
+export function Home() {
+	return (
+		<>
+			<Header />
+			<StyledHome>
+				{/* <div className="swiperCards"></div> */}
+				{/* <CardList /> */}
+				{/* <footer className="homeFooter"></footer> */}
+			</StyledHome>
+		</>
+	);
+}
