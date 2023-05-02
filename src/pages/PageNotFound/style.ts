0c4a18fd2d5269895d8f8e35179d95ee18476090
Amanda Dolci <amanda.dolci@gmail.com>
2023-05-02 12:35:54 -0300

    feat: add pagenotfound structure & style

---
+++ src/pages/PageNotFound/style.ts
@@ -0,0 +1,71 @@
+import styled from "styled-components";
+
+export const StyledPageNotFound = styled.main`
+	width: 100%;
+	height: fit-content;
+	min-height: 100%;
+	display: flex;
+	flex-direction: column-reverse;
+	align-items: center;
+	justify-content: center;
+	padding: 0 2%;
+
+	.pageNotFoundImages {
+		.desktopImg {
+			display: none;
+		}
+		.mobileImg {
+		}
+	}
+	.pageNotFoundMessages {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: flex-start;
+		text-align: center;
+
+		big {
+			color: var(--secondary);
+			font-family: "Fredericka the Great", cursive;
+			font-size: 128px;
+		}
+		h1 {
+			font-size: 36px;
+		}
+		a {
+			font-size: 18px;
+			color: inherit;
+			padding: 12px 0;
+			:hover {
+				color: var(--secondary);
+				text-decoration: underline;
+			}
+		}
+	}
+	@media (min-width: 768.1px) {
+		flex-direction: row;
+		min-height: fit-content;
+		gap: 3.5rem;
+		padding: 2rem;
+		.pageNotFoundImages {
+			.desktopImg {
+				display: block;
+			}
+			.mobileImg {
+				display: none;
+			}
+		}
+		.pageNotFoundMessages {
+			big {
+				font-size: 8rem;
+			}
+			h1 {
+				font-size: 2.25rem;
+			}
+			a {
+				font-size: 1.125rem;
+				padding: 0.75rem 0;
+			}
+		}
+	}
+`;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Login } from "../pages/Login";
 import { Register } from "../pages/Register";
+import { PageNotFound } from "../pages/PageNotFound";
 
 export function MainRoutes() {
 	return (
@@ -8,6 +9,7 @@ export function MainRoutes() {
 			<Route path="/login" element={<Login />} />
 			<Route path="/register" element={<Register />} />
 			<Route path="/" element={<Navigate to="/login" />} />
+			<Route path="*" element={<PageNotFound />} />
 		</Routes>
 	);
 }
