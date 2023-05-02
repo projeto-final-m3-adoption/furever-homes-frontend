1af129a184bc453b1426f133b79621609f02079f
bitoller <bi_toller@hotmail.com>
2023-05-02 16:05:53 -0300

    refactor: delete unused files

--- src/formsSchema/loginSchema.tsx
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const loginSchema = z.object({
-  email: z
-    .string()
-    .nonempty({ message: "O e-mail é obrigatório" })
-    .email({ message: "O e-mail é obrigatório" }),
-  password: z.string().min(1, { message: "A senha é obrigatória" }),
-});
