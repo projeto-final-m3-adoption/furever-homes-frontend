332efbb3f6b676a64e7661ffbd862ddbeb5b4446
bitoller <bi_toller@hotmail.com>
2023-05-04 10:59:34 -0300

    feat: add profile links

--- src/components/Footer/style.ts
+++ src/components/Footer/style.ts
@@ -1,36 +1,41 @@
 import styled from "styled-components";
 
 export const StyledFooter = styled.footer`
-	width: 100%;
-	height: 3.125rem;
-	background-color: var(--grey-0);
-	display: flex;
-	justify-content: center;
-	align-items: center;
+  width: 100%;
+  height: 3.125rem;
+  background-color: var(--grey-0);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-	.footer-paragraph {
-		font-size: 0.75rem;
-		color: var(--grey-6);
-		display: flex;
-		align-items: center;
-		gap: 0.313rem;
-	}
+  .footer-paragraph {
+    font-size: 0.75rem;
+    color: var(--grey-6);
+    display: flex;
+    align-items: center;
+    gap: 0.313rem;
+  }
 
-	.footer-heart {
-		height: 0.75rem;
-		width: auto;
-	}
+  .footer-paragraph > a {
+    text-decoration: none;
+	color: var(--grey-6);
+  }
 
-	@media (min-width: 768.1px) {
-		height: 4.563rem;
+  .footer-heart {
+    height: 0.75rem;
+    width: auto;
+  }
 
-		.footer-paragraph {
-			font-size: 1.125rem;
-		}
+  @media (min-width: 768.1px) {
+    height: 4.563rem;
 
-		.footer-heart {
-			height: 1.25rem;
-			width: auto;
-		}
-	}
+    .footer-paragraph {
+      font-size: 1.125rem;
+    }
+
+    .footer-heart {
+      height: 1.25rem;
+      width: auto;
+    }
+  }
 `;
