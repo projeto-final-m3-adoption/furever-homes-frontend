d796f1eb5e106f6ae3a65046a1299d48f294c8c3
BrunoAPdaC <brunoaugusto018@gmail.com>
2023-05-04 17:04:44 -0300

    fix:change in new pet modal

--- src/components/Form/RegisterNewPetForm/index.tsx
+++ src/components/Form/RegisterNewPetForm/index.tsx
@@ -4,6 +4,7 @@ import { registerNewPetFormSchema } from "./registerNewPetFromSchema";
 import { IRegisterNewPetFormData, petContext } from "../../../providers/petContext";
 import { Input } from "../../Form/Input";
 import { useContext } from "react";
+import { StyleModalNewPet } from "./style";
 
 export function RegisterNewPetForm() {
   const { createNewPet, openModalNewPet, setOpenModalNewPet } = useContext(petContext);
@@ -17,9 +18,12 @@ export function RegisterNewPetForm() {
   });
 
   return openModalNewPet === true ? (
-    <div role="dialog" className="modalNewPet">
+    <StyleModalNewPet role="dialog" className="modalNewPet">
       <form onSubmit={handleSubmit(createNewPet)}>
-        <button onClick={() => setOpenModalNewPet(false)}>X</button>
+        <div className="headerModal">
+          <h2>Cadastro Pet</h2>
+          <button onClick={() => setOpenModalNewPet(false)}>X</button>
+        </div>
         <Input
           id="name"
           label="Nome do pet"
@@ -29,27 +33,27 @@ export function RegisterNewPetForm() {
           {...register("name")}
         />
         <label htmlFor="type">Tipo do pet</label>
-        <select id="type" {...register("type")}>
+        <select id="type" {...register("type")} className="selectNewPet">
           <option value="">Escolha o tipo de pet aqui...</option>
           <option value="Cachorro">Cachorro</option>
           <option value="Gato">Gato</option>
           <option value="Outros">Outros</option>
         </select>
         <label htmlFor="age">Idade do pet</label>
-        <select id="age" {...register("age")}>
+        <select id="age" {...register("age")} className="selectNewPet">
           <option value="">Escolha a faixa etária do pet aqui...</option>
           <option value="Filhote">Filhote</option>
           <option value="Adulto">Adulto</option>
           <option value="Idoso">Idoso</option>
         </select>
         <label htmlFor="gender">Sexo do pet</label>
-        <select id="gender" {...register("gender")}>
+        <select id="gender" {...register("gender")} className="selectNewPet">
           <option value="">Escolha o sexo do pet aqui...</option>
           <option value="Femêa">Femêa</option>
           <option value="Macho">Macho</option>
         </select>
         <label htmlFor="size">Tamanho do pet</label>
-        <select id="size" {...register("size")}>
+        <select id="size" {...register("size")} className="selectNewPet">
           <option value="">Escolha o tamanho do pet aqui...</option>
           <option value="Pequeno">Pequeno</option>
           <option value="Médio">Médio</option>
@@ -63,14 +67,14 @@ export function RegisterNewPetForm() {
           error={errors.img?.message}
           {...register("img")}
         />
-          <Input
-            id="address"
-            label="Endereço do pet"
-            type="text"
-            placeholder="Digite aqui o endereço do pet..."
-            error={errors.address?.message}
-            {...register("address")}
-          />
+        <Input
+          id="address"
+          label="Endereço do pet"
+          type="text"
+          placeholder="Digite aqui o endereço do pet..."
+          error={errors.address?.message}
+          {...register("address")}
+        />
         <Input
           id="description"
           label="Conte mais sobre o seu pet aqui"
@@ -79,8 +83,10 @@ export function RegisterNewPetForm() {
           error={errors.description?.message}
           {...register("description")}
         />
-        <button type="submit">Cadastrar Pet</button>
+        <button type="submit" className="buttonNewPet">
+          Arranjar uma nova família para o pet
+        </button>
       </form>
-    </div>
+    </StyleModalNewPet>
   ) : null;
 }
