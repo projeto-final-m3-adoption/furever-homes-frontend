cc27a154e408fb76ed60f3f55e547ecc362114d8
bitoller <bi_toller@hotmail.com>
2023-05-05 18:48:39 -0300

    refactor: code indentation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -17,57 +17,57 @@ import { AdoptedModal } from "../../components/Modal/AdoptedModal";
 import { RegisterPetModal } from "../../components/Modal/RegisterPetModal";
 
 export function Home() {
-	const { registerPetModal, petDetailsModal, petObject, adoptedModal } =
-		useContext(petContext);
-	const { loginModal, token } = useContext(UserContext);
+  const { registerPetModal, petDetailsModal, petObject, adoptedModal } =
+    useContext(petContext);
+  const { loginModal, token } = useContext(UserContext);
 
-	return (
-		<>
-			<Header />
-			<StyledHome>
-				<Carousel />
-				<section className="petListContainer">
-					<div className="petListHeader">
-						<div className="petListFilters">
-							<IconButton
-								button={true}
-								img={dogIcon}
-								alt={"Filtrar por cachorros"}
-								size={"4rem"}
-								id={"dogIcon"}
-							/>
-							<IconButton
-								button={true}
-								img={catIcon}
-								alt={"Filtrar por gatos"}
-								size={"4rem"}
-								id={"catIcon"}
-							/>
-							<IconButton
-								button={true}
-								img={rabbitIcon}
-								alt={"Filtrar por outros"}
-								size={"4rem"}
-								id={"rabbitIcon"}
-							/>
-						</div>
-						<IconButton
-							button={true}
-							img={resetButton}
-							alt={"Resetar filtro"}
-							size={"4rem"}
-							id={"buttonReset"}
-						/>
-					</div>
-					<PetList />
-				</section>
-			</StyledHome>
-			<Footer />
-			{registerPetModal ? <RegisterPetModal /> : null}
-			{petDetailsModal ? <PetDetailsModal /> : null}
-			{token && petObject ? <PetDetailsModal /> : null}
-			{loginModal ? <LoginModal /> : null}
-			{adoptedModal ? <AdoptedModal /> : null}
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <StyledHome>
+        <Carousel />
+        <section className="petListContainer">
+          <div className="petListHeader">
+            <div className="petListFilters">
+              <IconButton
+                button={true}
+                img={dogIcon}
+                alt={"Filtrar por cachorros"}
+                size={"4rem"}
+                id={"dogIcon"}
+              />
+              <IconButton
+                button={true}
+                img={catIcon}
+                alt={"Filtrar por gatos"}
+                size={"4rem"}
+                id={"catIcon"}
+              />
+              <IconButton
+                button={true}
+                img={rabbitIcon}
+                alt={"Filtrar por outros"}
+                size={"4rem"}
+                id={"rabbitIcon"}
+              />
+            </div>
+            <IconButton
+              button={true}
+              img={resetButton}
+              alt={"Resetar filtro"}
+              size={"4rem"}
+              id={"buttonReset"}
+            />
+          </div>
+          <PetList />
+        </section>
+      </StyledHome>
+      <Footer />
+      {registerPetModal ? <RegisterPetModal /> : null}
+      {petDetailsModal ? <PetDetailsModal /> : null}
+      {token && petObject ? <PetDetailsModal /> : null}
+      {loginModal ? <LoginModal /> : null}
+      {adoptedModal ? <AdoptedModal /> : null}
+    </>
+  );
 }
